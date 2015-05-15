17e307a696843969e0e9b297a8a76ce58d3b2f64
Gino Crecco <GrandmaFunk@users.noreply.github.com>
2015-05-15 01:52:29 -0400

    Give sprite jumps on click

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function start_game() {
 	//load elements
 	sprite = document.getElementById("sprite");
-
+	jump = 0;
 	var x = parseInt(sprite.getAttribute("cx").slice(0,-1));
 	move_sprite(4, .5, x );
 }
@@ -9,10 +9,25 @@ function start_game() {
 function move_sprite(velocity, gravity, x) {
 	var new_x = x + velocity;
 	if (new_x < 50) {
-		velocity = velocity + gravity;
+		velocity = velocity + gravity - jump;
 	} else {
-		velocity = velocity - gravity;
+		velocity = velocity - gravity + jump;
 	}
+
+	//spring motion
+	if (velocity - gravity * 2 > 0) {
+		velocity -= 0.2;
+	}
+
+	//reset any jumps
+	if (jump !== 0) {
+		jump = 0;
+	}
+
 	sprite.setAttribute("cx", new_x + "%");
 	window.setTimeout(move_sprite, 100, velocity, gravity, new_x);
+}
+
+function entry() {
+	jump = 3;
 }
