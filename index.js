e25d60296dc83d223968ba0b07496905c07e542d
Gino Crecco <GrandmaFunk@users.noreply.github.com>
2015-05-15 18:20:38 -0400

    Make sprite bounce off window boundary

--- index.js
+++ index.js
@@ -7,23 +7,40 @@ function start_game() {
 			//velocity, gravity, x
 }
 
+function check_bounds(x, velocity) {
+	//return true if sprite is in bounds
+	if (0 < x + velocity && x + velocity < 100) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 function move_sprite(velocity, gravity, x) {
-	var new_x = x + velocity;
+	var new_x = x + velocity;	//sets x
+
+	//rest of the function gets new variables for next function call
 
 	//swing back and forth
 	if (new_x < 50) {
-		velocity = velocity + gravity - jump;
+		if (!check_bounds(new_x, velocity)) {
+			velocity = Math.abs(velocity) + gravity - jump;
+		}else {
+			velocity = velocity + gravity - jump;
+		}
 	} else {
-		velocity = velocity - gravity + jump;
+		if (!check_bounds(new_x, velocity)) {
+			velocity = -(velocity) - gravity + jump;
+		} else {
+			velocity = velocity - gravity + jump;
+		}
 	}
-
-	//spring towards center
 
 	//reset any jumps
 	if (jump !== 0) {
 		jump = 0;
 	}
-
+	
 	sprite.setAttribute("cx", new_x + "%");
 	//window.setTimeout(move_sprite, 50, velocity, gravity, new_x);
 	window.requestAnimationFrame( function() {
