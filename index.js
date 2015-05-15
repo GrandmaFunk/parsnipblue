24f64dcc137df102827e78c26cf483cf3e47fdde
Gino Crecco <GrandmaFunk@users.noreply.github.com>
2015-05-15 00:43:16 -0400

    Give sprite pendulum motion

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function start_game() {
 	//load elements
 	sprite = document.getElementById("sprite");
-	gravity = .5;
-	velocity = 5;
-//	move_sprite();
+
+	var x = parseInt(sprite.getAttribute("cx").slice(0,-1));
+	move_sprite(4, .5, x );
 }
 
-function move_sprite() {
-	var x = parseInt(sprite.getAttribute("cx").slice(0,-1));
+function move_sprite(velocity, gravity, x) {
 	var new_x = x + velocity;
-	velocity = velocity - gravity;
+	if (new_x < 50) {
+		velocity = velocity + gravity;
+	} else {
+		velocity = velocity - gravity;
+	}
 	sprite.setAttribute("cx", new_x + "%");
-	window.setTimeout(move_sprite, 100);
+	window.setTimeout(move_sprite, 100, velocity, gravity, new_x);
 }
