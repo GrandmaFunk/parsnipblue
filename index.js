1edeebc34ed41e268ec8b83ba4b266b92e857b51
Gino Crecco <GrandmaFunk@users.noreply.github.com>
2015-05-15 21:59:05 -0400

    Update score on crossover of center

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function start_game() {
 	jump = false;
 	jump_x = 2;
 	var x = parseInt(sprite.getAttribute("cx").slice(0,-1));
-	move_sprite(2, 0.1, x );
-			//velocity, gravity, x
+	move_sprite(2, 0.1, x, false);
+			//velocity, gravity, x, side
 }
 
 function check_bounds(x, velocity) {
@@ -17,10 +17,22 @@ function check_bounds(x, velocity) {
 	}
 }
 
-function move_sprite(velocity, gravity, x) {
+function apply_score() {
+	var score = document.getElementById("score");
+	score.textContent = parseInt(score.textContent) + 1;
+}
 
-	//gets new velocity to swing sprite from center
+function move_sprite(velocity, gravity, x, left_side) {
+	//moves sprite, requestAnimationFrame calls this function repetedly
+
 	if (x < 50) {
+		//sprite is on left side
+		if (left_side !== true) {
+			//apply score if not yet applied
+			left_side = true;
+			apply_score();
+		}
+		//keep in bounds
 		if (!check_bounds(x, velocity)) {
 			velocity = Math.abs(velocity) + gravity;
 		}else {
@@ -28,15 +40,22 @@ function move_sprite(velocity, gravity, x) {
 		}
 		//if jump on left side
 		if (velocity > 0 && jump) {
-			//if going towards center, move out
+			//if going towards center, move sprite towards bounds
 			velocity = velocity - jump_x;
 			jump = false;
 		}else if (jump) {
-			//if going towards bounds, move in
+			//if going towards bounds, move sprite towards center
 			velocity = velocity + jump_x;
 			jump = false;
 		}
 	} else {
+		//sprite on right side
+		if (left_side !== false) {
+			//apply score if not yet applied
+			left_side = false;
+			apply_score();
+		}
+		//keep in bounds
 		if (!check_bounds(x, velocity)) {
 			velocity = -(velocity) - gravity;
 		} else {
@@ -44,21 +63,22 @@ function move_sprite(velocity, gravity, x) {
 		}
 		//if jump on right side
 		if (jump && velocity < 0) {
-			//if going towards center
+			//if going towards center, move in opposite direction
 			velocity = velocity + jump_x;
 			jump = false;
 		} else if (jump) {
-			//if going towards bounds
+			//if going towards bounds, move in opposite direction
 			velocity = velocity - jump_x;
 			jump = false;
 		}
 	}
-
+	
+	//set new position
 	var new_x = x + velocity;	//sets x
 	sprite.setAttribute("cx", new_x + "%");
 	
 	window.requestAnimationFrame( function() {
-		move_sprite(velocity, gravity, new_x);
+		move_sprite(velocity, gravity, new_x, left_side);
 	});
 }
 
